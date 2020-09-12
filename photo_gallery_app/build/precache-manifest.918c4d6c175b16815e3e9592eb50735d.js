093eeb6105ad5c41386be3e7709089b9dbb9b9b4
Sohaib <55838991+Sohaib-01@users.noreply.github.com>
2020-09-12 13:35:37 +0100

    Photo Gallery App
    Added files for the completed photo gallery app

---
+++ photo_gallery_app/build/precache-manifest.918c4d6c175b16815e3e9592eb50735d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df4fe30dec0b0b5085a75dd5ef1e1da7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eac96c0f7530155bf3aa",
+    "url": "/static/css/2.62390166.chunk.css"
+  },
+  {
+    "revision": "201a30bfee3122722c84",
+    "url": "/static/css/main.962d1009.chunk.css"
+  },
+  {
+    "revision": "eac96c0f7530155bf3aa",
+    "url": "/static/js/2.ffb7c375.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.ffb7c375.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "201a30bfee3122722c84",
+    "url": "/static/js/main.de3eb8ad.chunk.js"
+  },
+  {
+    "revision": "bc2144d91206f9fab785",
+    "url": "/static/js/runtime-main.e4912f7f.js"
+  }
+]);
